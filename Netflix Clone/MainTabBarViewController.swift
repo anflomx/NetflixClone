d35f1df340ebf52366ec7d8ac1ea5d9e4bb932d1
Antonio <aff128312@gmail.com>
2021-12-11 09:36:41 -0600

    add uncommit files

--- Netflix Clone/MainTabBarViewController.swift
+++ Netflix Clone/MainTabBarViewController.swift
@@ -7,10 +7,11 @@
 
 import UIKit
 
-class MainTabBarViewController: UIViewController {
+class MainTabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemYellow
     }
 }
 
