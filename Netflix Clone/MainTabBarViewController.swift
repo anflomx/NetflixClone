fed601f9c7d484769a7a4fb2156d731855295f8e
Antonio <aff128312@gmail.com>
2021-12-11 10:07:43 -0600

    Setup views and Icons

--- Netflix Clone/MainTabBarViewController.swift
+++ Netflix Clone/MainTabBarViewController.swift
@@ -18,6 +18,17 @@ class MainTabBarViewController: UITabBarController {
         let searchVC = UINavigationController(rootViewController: SearchViewController())
         let downloadsVC = UINavigationController(rootViewController: DownloadsViewController())
         
+        homeVC.tabBarItem.image = UIImage(systemName: "house")
+        upcomingVC.tabBarItem.image = UIImage(systemName: "play.circle")
+        searchVC.tabBarItem.image = UIImage(systemName: "magnifyingglass")
+        downloadsVC.tabBarItem.image = UIImage(systemName: "arrow.down.to.line")
+        
+        homeVC.title = "Home"
+        upcomingVC.title = "Coming Soon"
+        searchVC.title = "Top Search"
+        downloadsVC.title = "Downloads"
+        
+        tabBar.tintColor = .label
         
         setViewControllers([homeVC, upcomingVC, searchVC, downloadsVC], animated: true)
     }
