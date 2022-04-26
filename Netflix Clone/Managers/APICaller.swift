ef9b34c683b8bb103b423ebb544c3584632820fa
Antonio <aff128312@gmail.com>
2022-04-25 23:24:36 -0500

    Add Networking Layer

---
+++ Netflix Clone/Managers/APICaller.swift
@@ -0,0 +1,40 @@
+//
+//  APICaller.swift
+//  Netflix Clone
+//
+//  Created by Antonio Flores on 25/04/22.
+//
+
+import Foundation
+
+struct Constants {
+    static let API_KEY = "34959eafdfa19c23c27e72032dfe3e30"
+    static let baseURL = "https://api.themoviedb.org"
+}
+
+enum APIError: Error {
+    case failedTogetData
+}
+
+class APICaller {
+    static let shared = APICaller()
+    
+    func getTrendingMovies(completion: @escaping (Result<[Movie], Error>) -> Void) {
+        guard let url = URL(string: "\(Constants.baseURL)/3/trending/all/day?api_key=\(Constants.API_KEY)") else { return }
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            do {
+                let results = try JSONDecoder().decode(TrendingMoviesResponse.self, from: data)
+                completion(.success(results.results))
+                
+            } catch {
+                completion(.failure(error))
+            }
+        }
+        task.resume()
+    }
+}
