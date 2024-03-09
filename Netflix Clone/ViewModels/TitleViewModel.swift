f07c158a5b72b3f6f19f18486b8cd4d512d02cfe
Antonio <aff128312@gmail.com>
2024-03-09 15:10:51 -0600

    Ep.8 Setup UpcomingVC

---
+++ Netflix Clone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  Netflix Clone
+//
+//  Created by Antonio Flores on 09/03/24.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
