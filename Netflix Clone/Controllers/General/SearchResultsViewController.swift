da14ea1d6680b9d4b79c82a472c0bee3abd08981
Antonio <aff128312@gmail.com>
2024-03-12 23:04:37 -0600

    Ep.13 Navigate to TitlePreviewVC from others VC

--- Netflix Clone/Controllers/General/SearchResultsViewController.swift
+++ Netflix Clone/Controllers/General/SearchResultsViewController.swift
@@ -7,8 +7,13 @@
 
 import UIKit
 
+protocol SearchResultsViewControllerDelegate: AnyObject {
+    func searchResultsViewControllerDidTapItem(_ viewModel: TitlePreviewViewModel)
+}
+
 class SearchResultsViewController: UIViewController {
     public var titles: [Title] = [Title]()
+    public weak var delegate: SearchResultsViewControllerDelegate?
     
     public let searchResultsCollectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
@@ -47,5 +52,21 @@ extension SearchResultsViewController: UICollectionViewDataSource, UICollectionV
         let title = titles[indexPath.row]
         cell.configure(with: title.poster_path ?? "")
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+        let title = titles[indexPath.row]
+        let titleName = title.original_title ?? ""
+        APICaller.shared.getMovie(query: titleName + " trailer") { [weak self] result in
+            switch result {
+            case .success(let videoElement):
+                self?.delegate?.searchResultsViewControllerDidTapItem(TitlePreviewViewModel(title: titleName,
+                                                                youtubeView: videoElement,
+                                                                titleOverview: title.overview ?? ""))
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
 }
